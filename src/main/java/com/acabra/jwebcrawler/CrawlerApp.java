60b03fe271cd25a7f4d5ad38d8717ced4d24f0f0
Agustin Cabra <cabra.agustin@gmail.com>
2020-09-06 23:21:30 +0200

    Configuring base dependencies
    1. Maven plugins for jar packaging.
    2. Simple logging sl4j
    3. Junit unit testing
    4. jacoco code coverage

---
+++ src/main/java/com/acabra/jwebcrawler/CrawlerApp.java
@@ -0,0 +1,26 @@
+package com.acabra.jwebcrawler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CrawlerApp {
+
+    private final Logger logger = LoggerFactory.getLogger(CrawlerApp.class);
+
+    public CrawlerApp() {
+    }
+
+    void start(String subDomain) {
+        if (!subDomain.trim().isEmpty()) {
+            logger.info(String.format("Will attempt crawl for pages of SubDomain :<%s>", subDomain));
+        }
+    }
+
+    public static void main(String... args) {
+        if (args != null && args.length > 0) {
+            new CrawlerApp().start(args[0]);
+        } else {
+            throw new NullPointerException("Sub-domain not found: expected sub-domain passed as argument.");
+        }
+    }
+}
